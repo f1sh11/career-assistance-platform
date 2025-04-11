83a78773cdbbeb27f05f963c1886ba68fd172a6c
f1sh11 <jijoinyucheng@gmail.com>
2025-04-11 13:46:03 +0800

    登录过时改为每一小时自动登出

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ const logDirectory = path.join(__dirname, '../../src/logs');
 // Generate JWT token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: '1h' 
   });
 };
 
