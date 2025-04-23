692ece8d14dee98b9d32728b83f8ba6a21414350
Tian123454321 <luotianyi190@gmail.com>
2025-04-23 23:27:40 +0800

    Fixed homepage connect

--- app/page.js
+++ app/page.js
@@ -44,7 +44,7 @@ export default function Home() {
               Career Assistance Platform
             </p>
             <button
-              onClick={() => handleMenuClick("/resources")}
+              onClick={() => handleMenuClick("/login")}
               className="mt-8 px-6 py-2 bg-yellow-400 text-black font-medium rounded shadow hover:bg-yellow-500 transition"
             >
               Get Started
