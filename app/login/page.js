b642047dff3d2c654dfa8d28f3808812766d72a9
f1sh11 <jijoinyucheng@gmail.com>
2025-05-18 21:19:05 +0800

    initial role bug fix

--- app/login/page.js
+++ app/login/page.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 
 export default function LoginPage() {
-  const [role, setRole] = useState("");
+  const [role, setRole] = useState("student");
   const [identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -77,7 +77,6 @@ export default function LoginPage() {
           }}
           className="border p-2 w-full rounded mb-4 text-black"
         >
-          <option value="">Select your identity</option>
           <option value="student">Student</option>
           <option value="mentor">Mentor</option>
           <option value="industry">Industry</option>
